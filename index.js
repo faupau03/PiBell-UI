cc2509df7c3a695d4cae2d6cac839dde73ffee4a
Paul <paul.paffe@gmx.de>
2022-01-02 22:06:55 +0100

    Initial commit with express.js tailwindcss and docker-compose

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 80
+const path = require('path')
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/public'));
+
+app.get("/", (request, response) => {
+  response.render('index.ejs', {
+    doorbell_ip: process.env.DOORBELL_IP,
+    doorbell_port: process.env.DOORBELL_PORT,
+  })
+});
+
+app.get("/output.css", (request, response) => {
+  response.sendFile(__dirname + '/public/output.css');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
